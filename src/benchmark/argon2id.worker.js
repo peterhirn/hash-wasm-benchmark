c0690d99d30c9865cc4cbae959c2657b74087622
Peter Hirn <peter.hirn@phi.ag>
2025-04-04 12:04:37 +0200

    Add @phi-ag/argon2

--- src/benchmark/argon2id.worker.js
+++ src/benchmark/argon2id.worker.js
@@ -3,15 +3,26 @@ import { argon2id as wasmArgon2 } from 'hash-wasm';
 import argon2WasmPro from 'argon2-wasm-pro';
 import argon2Wasm from 'argon2-wasm';
 import argon2Browser from 'argon2-browser/lib/argon2';
-import { getVersion } from '../utils';
+import * as phiArgon2 from "@phi-ag/argon2";
+import phiInitialize from "@phi-ag/argon2/fetch";
+import phiWasm from "@phi-ag/argon2/argon2.wasm";
+import { getVersion, toHex } from "../utils";
 
 const memorySize = 512;
 const iterations = 8;
 const hashLength = 32;
 const parallelism = 1;
 const salt = 'MYSALT123';
+const saltUint8Array = new TextEncoder().encode(salt);
 
-const suite = new Bench([{ size: 16, divisor: 1, showIterations: true }]);
+let phiArgon2Initialized = null;
+
+const suite = new Bench(
+  [{ size: 16, divisor: 1, showIterations: true }],
+  async () => {
+    phiArgon2Initialized = await phiInitialize(phiWasm);
+  },
+);
 
 suite.addAsync(`hash-wasm ${getVersion('hash-wasm')}`, (buf) => {
   return wasmArgon2({
@@ -61,4 +72,16 @@ suite.addAsync(`argon2-browser ${getVersion('argon2-browser')}`, async (buf) => 
     type: argon2Browser.ArgonType.Argon2id,
   });
   return hash.hashHex;
+});
+
+suite.addSync(`@phi-ag/argon2 ${getVersion("@phi-ag/argon2")}`, (buf) => {
+  const hash = phiArgon2Initialized.hash(buf, {
+    salt: saltUint8Array,
+    timeCost: iterations,
+    memoryCost: memorySize,
+    hashLength,
+    parallelism,
+    type: phiArgon2.Argon2Type.Argon2id,
+  });
+  return toHex(hash.hash);
 });
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,11 @@ import packageJSON from '../package.json';
 export function getVersion(name) {
   return packageJSON.dependencies[name] ?? '';
 }
+
+export function toHex(array) {
+  let result = '';
+  for (const value of array) {
+    result += value.toString(16).padStart(2, '0');
+  }
+  return result;
+};
